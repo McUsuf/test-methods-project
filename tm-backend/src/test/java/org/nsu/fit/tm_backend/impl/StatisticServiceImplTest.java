14fc7e289c093f1d22e5a63ecd56e2fcd911d672
druzhinin <saske12321@gmail.com>
2022-11-28 18:22:14 +0700

    stat

--- tm-backend/src/test/java/org/nsu/fit/tm_backend/impl/StatisticServiceImplTest.java
+++ tm-backend/src/test/java/org/nsu/fit/tm_backend/impl/StatisticServiceImplTest.java
@@ -1,6 +1,7 @@
 package org.nsu.fit.tm_backend.impl;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -38,30 +39,59 @@ public class StatisticServiceImplTest {
 
     @Mock
     private CustomerServiceImpl customerService;
-    @Test
-    void testCalculateWithId(){
-        UUID customerUuid = UUID.randomUUID();
-        Set<UUID> subscriptionUuids=new HashSet<UUID>();
-        UUID subscriptionUuid = new UUID(0,0);
-        List<SubscriptionPojo>subscriptionPojos= new ArrayList<>();
-            CustomerPojo customerPojo=new CustomerPojo();
-            customerPojo.id=customerUuid;
-            customerPojo.firstName = "John";
-            customerPojo.lastName = "Wick";
-            customerPojo.login = "john_wick@example.com";
-            customerPojo.pass = "1234567";
-            customerPojo.balance = 20;
+
+    CustomerPojo customerPojo;
+    StatisticPerCustomerBO statisticPerCustomerBO;
+    Set<UUID> setCustomerUuids;
+    Set<UUID> subscriptionUuids;
+    UUID subscriptionUuid;
+    List<UUID> listCustomerUuids;
+    SubscriptionPojo subscriptionPojo;
+    List<SubscriptionPojo>subscriptionPojos;
+    UUID customerUuid;
+    StatisticBO statisticBO;
+    @BeforeEach
+    public void initEach(){
+        listCustomerUuids=new ArrayList<>();
+        setCustomerUuids=new HashSet<UUID>();
+        subscriptionUuids=new HashSet<UUID>();
+        customerPojo = new CustomerPojo();
+        customerUuid = UUID.randomUUID();
+        statisticPerCustomerBO = new StatisticPerCustomerBO();
+        subscriptionPojos= new ArrayList<>();
+        customerPojo.id=customerUuid;
+        customerPojo.firstName = "John";
+        customerPojo.lastName = "Wick";
+        customerPojo.login = "john_wick@example.com";
+        customerPojo.pass = "1234567";
+        customerPojo.balance = 20;
+        setCustomerUuids.add(customerUuid);
+
+        listCustomerUuids.add(customerUuid);
+
+        statisticPerCustomerBO.setOverallBalance(20);
+        statisticPerCustomerBO.setOverallFee(30);
+        statisticPerCustomerBO.setSubscriptionIds(subscriptionUuids);
         for(int i=0;i<3;i++){
-            SubscriptionPojo subscriptionPojo = new SubscriptionPojo();
+            subscriptionPojo = new SubscriptionPojo();
             subscriptionPojo.planName="test";
             subscriptionPojo.planDetails="test";
             subscriptionUuid = UUID.randomUUID();
             subscriptionPojo.id=subscriptionUuid;
             subscriptionPojo.planFee=10;
             subscriptionUuids.add(subscriptionUuid);
-            subscriptionPojo.customerId=customerUuid;
+            subscriptionPojo.customerId=listCustomerUuids.get(0);
             subscriptionPojos.add(subscriptionPojo);
         }
+        Set <StatisticPerCustomerBO> statisticPerCustomerBOS= new HashSet<StatisticPerCustomerBO>();
+        statisticPerCustomerBOS.add(statisticPerCustomerBO);
+        statisticBO = new StatisticBO();
+        statisticBO.setCustomers(statisticPerCustomerBOS);
+        statisticBO.setOverallBalance(20);
+        statisticBO.setOverallFee(30);
+    }
+    @Test
+    void testCalculateWithId(){
         Mockito.when(customerService.lookupCustomer(customerUuid)).thenAnswer(
                 (Answer) invocation -> {
                     Object[] args = invocation.getArguments();
@@ -70,18 +100,12 @@ public class StatisticServiceImplTest {
                 }
         );
         Mockito.when(subscriptionService.getSubscriptions(customerUuid)).thenReturn(subscriptionPojos);
-        StatisticPerCustomerBO statisticPerCustomerBO = new StatisticPerCustomerBO();
-        statisticPerCustomerBO.setOverallBalance(20);
-        statisticPerCustomerBO.setOverallFee(30);
-        statisticPerCustomerBO.setSubscriptionIds(subscriptionUuids);
         Assertions.assertEquals(statisticService.calculate(customerPojo.id),statisticPerCustomerBO);
     }
 
     @Test
     void testCalculateWithIdWhereCustomerIsNull(){
-        UUID customerUuid = UUID.randomUUID();
-        CustomerPojo customerPojo=new CustomerPojo();
-        customerPojo.id=customerUuid;
+        statisticService=Mockito.spy(statisticService);
         Mockito.when(customerService.lookupCustomer(customerUuid)).thenAnswer(
                 (Answer) invocation -> {
                     Object[] args = invocation.getArguments();
@@ -94,38 +118,7 @@ public class StatisticServiceImplTest {
     }
     @Test
     void TestCalculateWithoutId(){
-        List<UUID> listCustomerUuids=new ArrayList<UUID>();
-        Set<UUID> setCustomerUuids=new HashSet<UUID>();
-        Set<UUID> subscriptionUuids=new HashSet<UUID>();
-        List<SubscriptionPojo>subscriptionPojos= new ArrayList<>();
-        UUID subscriptionUuid=null;
-            UUID customerUuid = UUID.randomUUID();
-            listCustomerUuids.add(customerUuid);
-            setCustomerUuids.add(customerUuid);
-            CustomerPojo customerPojo = new CustomerPojo();
-            customerPojo.id = customerUuid;
-            customerPojo.firstName = "John";
-            customerPojo.lastName = "Wick";
-            customerPojo.login = "john_wick@example.com";
-            customerPojo.pass = "1234567";
-            customerPojo.balance = 20;
-        for(int i=0;i<3;i++){
-            SubscriptionPojo subscriptionPojo = new SubscriptionPojo();
-            subscriptionPojo.planName="test";
-            subscriptionPojo.planDetails="test";
-            subscriptionUuid = UUID.randomUUID();
-            subscriptionPojo.id=subscriptionUuid;
-            subscriptionPojo.planFee=10;
-            subscriptionUuids.add(subscriptionUuid);
-            subscriptionPojo.customerId=listCustomerUuids.get(0);
-            subscriptionPojos.add(subscriptionPojo);
-        }
-        StatisticPerCustomerBO statisticPerCustomerBO = new StatisticPerCustomerBO();
-        statisticPerCustomerBO.setOverallBalance(20);
-        statisticPerCustomerBO.setOverallFee(30);
-        statisticPerCustomerBO.setSubscriptionIds(subscriptionUuids);
-        Set <StatisticPerCustomerBO> statisticPerCustomerBOS= new HashSet<StatisticPerCustomerBO>();
-        statisticPerCustomerBOS.add(statisticPerCustomerBO);
+        statisticService=Mockito.spy(statisticService);
         Mockito.when(customerService.getCustomerIds()).thenReturn(setCustomerUuids);
         Mockito.when(customerService.lookupCustomer(customerUuid)).thenAnswer(
                 (Answer) invocation -> {
@@ -135,25 +128,22 @@ public class StatisticServiceImplTest {
                 }
         );
         Mockito.when(subscriptionService.getSubscriptions(customerUuid)).thenReturn(subscriptionPojos);
-        StatisticBO statisticBO = new StatisticBO();
-        statisticBO.setCustomers(statisticPerCustomerBOS);
-        statisticBO.setOverallBalance(20);
-        statisticBO.setOverallFee(30);
         Assertions.assertEquals(statisticService.calculate(),statisticBO);
     }
     @Test
     void testCalculateWithoutIdWhereCustomerIsNull(){
-        Set <UUID> customersUuids= new HashSet<UUID>();
-        customersUuids.add(UUID.randomUUID());
-        UUID customerUuid = UUID.randomUUID();
-        CustomerPojo customerPojo=new CustomerPojo();
-        customerPojo.id=customerUuid;
-        Mockito.when(customerService.getCustomerIds()).thenReturn(customersUuids);
         Set<StatisticPerCustomerBO> statisticPerCustomerBOS = new HashSet<StatisticPerCustomerBO>();
         StatisticBO statisticBO = new StatisticBO();
         statisticBO.setCustomers(statisticPerCustomerBOS);
         statisticBO.setOverallFee(0);
         statisticBO.setOverallBalance(0);
+        Set <UUID> customersUuids= new HashSet<UUID>();
+        customersUuids.add(UUID.randomUUID());
+        customerPojo.id=customerUuid;
+
+        statisticService=Mockito.spy(statisticService);
+        Mockito.when(customerService.getCustomerIds()).thenReturn(customersUuids);
+
         Assertions.assertEquals(statisticService.calculate(),statisticBO);
     }
 }
